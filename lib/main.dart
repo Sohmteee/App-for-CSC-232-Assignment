4b3a0da8711f90156cb8f6951d601779abd4af12
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-18 07:03:27 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class _HomePageState extends State<HomePage> {
             ),
             const SizedBox(height: 20),
             CustomField(
-              icon: const Icon(Icons.sex),
+              icon: const Icon(Icons.),
               controller: regNo,
               hintText: "Reg Number",
             ),
