ff4ce6e99643c5634baa153257fef7cf5eaed276
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-18 07:16:11 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -86,7 +86,7 @@ class CustomField extends StatefulWidget {
 
   final String hintText;
   final TextEditingController controller;
-  final List<Icons?>? icon;
+  final Widget? icon;
 
   @override
   State<CustomField> createState() => _CustomFieldState();
