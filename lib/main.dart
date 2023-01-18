27a99017402f4a3f354edc8d61ca4c336d625e89
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-18 07:00:09 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _HomePageState extends State<HomePage> {
             ),
             const SizedBox(height: 20),
             CustomField(
-              icon: const Icon(Icons.home_max_rounded),
+              icon: const Icon(Icons.home),
               controller: faculty,
               hintText: "Faculty",
             ),
