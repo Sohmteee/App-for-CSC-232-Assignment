eab71ec2e7062dc66c532d6c5d929fffd21c1ab8
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-20 07:39:28 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -4,7 +4,6 @@ final TextEditingController firstName = TextEditingController();
 final TextEditingController lastName = TextEditingController();
 final TextEditingController regNo = TextEditingController();
 final TextEditingController department = TextEditingController();
-final TextEditingController faculty = TextEditingController();
 final TextEditingController stateOfOrigin = TextEditingController();
 final TextEditingController phoneNumber = TextEditingController();
 final TextEditingController email = TextEditingController();
