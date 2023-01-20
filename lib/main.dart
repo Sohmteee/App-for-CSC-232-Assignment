190dd2ff0659b9cde37556f195aa53607ae06af6
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-20 07:54:03 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -49,8 +49,8 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(height: 20),
             DecoratedBox(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-              ),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all()),
               child: DropdownButton(
                 value: sexValue,
                 items: sex.map((String items) {
