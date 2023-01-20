1c9da873d5f84b2c23dfae6f615d7e963163271e
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-20 07:39:53 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 final TextEditingController firstName = TextEditingController();
 final TextEditingController lastName = TextEditingController();
 final TextEditingController regNo = TextEditingController();
-final TextEditingController department = TextEditingController();
 final TextEditingController stateOfOrigin = TextEditingController();
 final TextEditingController phoneNumber = TextEditingController();
 final TextEditingController email = TextEditingController();
 
 List<String> sex = ["Male", "Female"];
+List<String> department = ["Male", "Female"];
