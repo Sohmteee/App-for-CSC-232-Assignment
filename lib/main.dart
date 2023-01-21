31658a827158ce3b02c8bf24ab31ea3b9a696580
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-21 08:02:05 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _HomePageState extends State<HomePage> {
             Row(
               children: [
                 SizedBox(
-                  width: 20,
+                  width: 50,
                   child: Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20),
