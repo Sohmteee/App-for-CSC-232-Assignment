e2409c9bf2181abc03536635054be33ad8bfad8c
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-22 11:01:54 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _HomePageState extends State<HomePage> {
                   ),
                   child: DropdownButton(
                     value: sexValue,
-                    
+                    hint: ,
                     items: sex.map((String items) {
                       return DropdownMenuItem(
                         value: items,
