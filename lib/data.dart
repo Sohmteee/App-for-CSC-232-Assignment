ceac502b2f81953c0a8a53603151946f4f61a3e5
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-23 11:49:22 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -6,7 +6,7 @@ final TextEditingController regNo = TextEditingController();
 final TextEditingController phoneNumber = TextEditingController();
 final TextEditingController email = TextEditingController();
 
-bool? bypass;
+bool bypass = false;
 
 String? sexValue;
 String? departmentValue;
