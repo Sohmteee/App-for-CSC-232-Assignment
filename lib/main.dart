cee7250bd9e64ab8c738daa533e0e38563c714a5
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-23 11:57:28 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -162,7 +162,7 @@ class _HomePageState extends State<HomePage> {
                 });
               },
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.circular(2),
               ),
             )
           ],
