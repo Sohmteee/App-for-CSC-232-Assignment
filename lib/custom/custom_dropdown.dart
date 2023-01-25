df701480a80fa720792f57b427cd1db2196da760
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-25 12:20:11 +0100

    commit

--- lib/custom/custom_dropdown.dart
+++ lib/custom/custom_dropdown.dart
@@ -1,2 +1,41 @@
 import 'package:flutter/material.dart';
 
+class CustomDropDown extends StatefulWidget {
+  const CustomDropDown({super.key});
+
+  @override
+  State<CustomDropDown> createState() => _CustomDropDownState();
+}
+
+class _CustomDropDownState extends State<CustomDropDown> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 120,
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: Colors.grey,
+        ),
+      ),
+      child: DropdownButton(
+        value: stateOfOriginValue,
+        hint: const Text("State of Origin"),
+        items: stateOfOrigin.map((String items) {
+          return DropdownMenuItem(
+            value: items,
+            child: Text(items),
+          );
+        }).toList(),
+        onChanged: (String? newValue) {
+          setState(() {
+            stateOfOriginValue = newValue!;
+          });
+        },
+        isExpanded: true,
+        underline: Container(),
+      ),
+    );
+  }
+}
