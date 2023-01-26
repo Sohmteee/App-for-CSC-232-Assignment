5b8340ad53480f440bb4bd073dd20cc0be65367d
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-26 13:09:51 +0100

    commit

--- lib/student.dart
+++ lib/student.dart
@@ -1,13 +1,14 @@
 import 'package:hive/hive.dart';
 
 class Student extends HiveObject {
-  String firstName, lastName, sex, email;
+  String firstName, lastName, sex, department, email;
   int regNumber, phone;
 
   Student({
     required this.firstName,
     required this.lastName,
     required this.sex,
+    required this.department,
     required this.firstName,
   });
 }
