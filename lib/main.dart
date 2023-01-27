d01a52f587c85c5ac98fdb448bfd438175bc6dea
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-27 16:15:27 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'screens/homepage.dart';
+import 'screens/homepage_screen.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -9,7 +9,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  
+
   const MyApp({super.key});
 
   @override
