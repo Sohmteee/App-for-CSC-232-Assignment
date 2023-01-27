cfd39ec02b738f9b0418ea729f8f2239858c660e
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-27 16:28:33 +0100

    commit

---
+++ lib/screens/data_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class DataScreen extends StatefulWidget {
+  const DataScreen({super.key});
+
+  @override
+  State<DataScreen> createState() => _DataScreenState();
+}
+
+class _DataScreenState extends State<DataScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [
+            const Center(
+              child: Text("Student List"),
+            ),
+            DataTable(columns: [DataColumn(label: Text("Reg Number"))], rows: rows)
+          ],
+        ),
+      ),
+    );
+  }
+}
