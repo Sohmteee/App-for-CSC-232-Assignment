703bca895c5cc93d2e1ee57dbfb3f9a65553708c
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-27 16:36:11 +0100

    commit

--- lib/screens/data_screen.dart
+++ lib/screens/data_screen.dart
@@ -44,7 +44,7 @@ class _DataScreenState extends State<DataScreen> {
                     label: Text("Email Address"),
                   ),
                 ],
-                rows: testStudentList.map((cell) => null).toList(),
+                rows: testStudentList.map((cell) => DataRow(cells: cells)).toList(),
               ),
             )
           ],
