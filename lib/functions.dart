6c265c1b9065cda654ff8255f1a553c27b0edb66
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-28 19:33:50 +0100

    commit

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,1 +1,10 @@
-clear
+void clearInfo() {
+  firstName.clear();
+  lastName.clear();
+  sexValue = null;
+  departmentValue = null;
+  stateOfOriginValue = null;
+  regNo.clear();
+  phoneNumber.clear();
+  email.clear();
+}
