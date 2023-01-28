1ffc4dd0e867854a9c6283e6c04f6aff8ddfa5b7
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-28 19:23:36 +0100

    commit

--- lib/screens/add_student_screen.dart
+++ lib/screens/add_student_screen.dart
@@ -15,6 +15,7 @@ class AddStudentScreen extends StatefulWidget {
 class _AddStudentScreenState extends State<AddStudentScreen> {
   @override
   Widget build(BuildContext context) {
+    
     @override
     void initState() {
       super.initState();
