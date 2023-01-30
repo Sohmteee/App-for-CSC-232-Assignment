786ec91733b4a40c995051955867e560eb05d8c7
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-30 22:21:36 +0100

    commit

--- lib/screens/data_screen.dart
+++ lib/screens/data_screen.dart
@@ -13,67 +13,67 @@ class _DataScreenState extends State<DataScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Expanded(
-            child: ListView(
-              children: [
-                DataTable(
-                  columns: const [
-                    DataColumn(
-                      label: Text("Reg Number"),
-                    ),
-                    DataColumn(
-                      label: Text("Name"),
-                    ),
-                    DataColumn(
-                      label: Text("Sex"),
-                    ),
-                    DataColumn(
-                      label: Text("Department"),
-                    ),
-                    DataColumn(
-                      label: Text("State of Origin"),
-                    ),
-                    DataColumn(
-                      label: Text("Phone Number"),
-                    ),
-                    DataColumn(
-                      label: Text("Email Address"),
-                    ),
-                  ],
-                  rows: testStudentList
-                      .map((student) => DataRow(
-                            cells: [
-                              DataCell(
-                                Text(student.regNumber.toString()),
-                              ),
-                              DataCell(
-                                Text(
-                                    "${student.firstName} ${student.lastName}"),
-                              ),
-                              DataCell(
-                                Text(student.sex),
-                              ),
-                              DataCell(
-                                Text(student.department),
-                              ),
-                              DataCell(
-                                Text(student.stateOfOrigin),
-                              ),
-                              DataCell(
-                                Text(student.phoneNumber),
-                              ),
-                              DataCell(
-                                Text(student.email),
-                              ),
-                            ],
-                          ))
-                      .toList(),
-                ),
-              ],
+        child: ListView(
+          children: [
+            const Center(
+              child: Text("Student List"),
             ),
-          ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                columns: const [
+                  DataColumn(
+                    label: Text("Reg Number"),
+                  ),
+                  DataColumn(
+                    label: Text("Name"),
+                  ),
+                  DataColumn(
+                    label: Text("Sex"),
+                  ),
+                  DataColumn(
+                    label: Text("Department"),
+                  ),
+                  DataColumn(
+                    label: Text("State of Origin"),
+                  ),
+                  DataColumn(
+                    label: Text("Phone Number"),
+                  ),
+                  DataColumn(
+                    label: Text("Email Address"),
+                  ),
+                ],
+                rows: testStudentList
+                    .map((student) => DataRow(
+                          cells: [
+                            DataCell(
+                              Text(student.regNumber.toString()),
+                            ),
+                            DataCell(
+                              Text("${student.firstName} ${student.lastName}"),
+                            ),
+                            DataCell(
+                              Text(student.sex),
+                            ),
+                            DataCell(
+                              Text(student.department),
+                            ),
+                            DataCell(
+                              Text(student.stateOfOrigin),
+                            ),
+                            DataCell(
+                              Text(student.phoneNumber),
+                            ),
+                            DataCell(
+                              Text(student.email),
+                            ),
+                          ],
+                        ))
+                    .toList(),
+              ),
+            )
+          ],
         ),
       ),
     );
