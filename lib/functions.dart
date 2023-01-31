dbb0419fd30c53a4004288f3449651f12060d3e5
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-01 00:21:18 +0100

    commit

--- lib/functions.dart
+++ lib/functions.dart
@@ -2,7 +2,7 @@ import 'data.dart';
 import 'student.dart';
 
 void clearInfo() {
-  firstName.value.clear();
+  firstName.clear();
   lastName.clear();
   sexValue = null;
   departmentValue = null;
