2a65f257085052b11fb098dc270ced1518917fbf
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-31 23:38:31 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 
-import 'student.dart';
-
 final TextEditingController firstName = TextEditingController();
 final TextEditingController lastName = TextEditingController();
 final TextEditingController regNo = TextEditingController();
