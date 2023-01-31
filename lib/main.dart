8de29500dc41a38c29a21c649028b4cbde940ed1
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-31 23:19:17 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_for_csc_232_assignment/data.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'screens/homepage_screen.dart';
@@ -6,8 +7,8 @@ void main() async {
   await Hive.initFlutter();
   Box box = await Hive.openBox('myBox');
 
-  if (box.get("studentsList") == null) box.put("userId", docUser.id);
-  userId = box.get("userId");
+  if (box.get("studentsList") == null) box.put("studentsList", []);
+  studentList = box.get("studentsList");
 
   runApp(const MyApp());
 }
