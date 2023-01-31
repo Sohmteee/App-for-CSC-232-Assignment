a1f472eb45f921490e05275aa1f68f1b8d0f0e74
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-31 23:59:48 +0100

    commit

--- lib/screens/add_student_screen.dart
+++ lib/screens/add_student_screen.dart
@@ -242,6 +242,7 @@ class _AddStudentScreenState extends State<AddStudentScreen> {
                         email: email.text.trim());
                     studentsList.add(student);
                     box.put("studentsList", studentsList);
+                    debugPrint(studentsList.toString());
 
                     Navigator.push(
                       context,
