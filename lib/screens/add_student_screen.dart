13af2f72cf36d0bafdf758b7f03f1ddace638f01
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-31 22:52:20 +0100

    commit

--- lib/screens/add_student_screen.dart
+++ lib/screens/add_student_screen.dart
@@ -44,7 +44,7 @@ class _AddStudentScreenState extends State<AddStudentScreen> {
       if (!departmentIsValid) return "Please enter your department";
       if (!stateOfOriginIsValid) return "Please enter your state of origin";
       if (!regNoIsValid) {
-        return (regNo.text.trim().length == 10)
+        return (regNo.text.trim().length != 10)
             ? "Please enter a valid reg number"
             : "Please enter your reg number";
       }
