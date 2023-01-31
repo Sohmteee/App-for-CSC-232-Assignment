08c8cb0136be5827de330636b80926563fa32e04
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-31 16:35:20 +0100

    commit

--- lib/screens/add_student_screen.dart
+++ lib/screens/add_student_screen.dart
@@ -31,7 +31,8 @@ class _AddStudentScreenState extends State<AddStudentScreen> {
       bool stateOfOriginIsValid =
           stateOfOriginValue != null && stateOfOriginValue != "";
       bool regNoIsValid =
-          regNo.text.trim().isNotEmpty && regNo.value.text.trim() != "";
+          (regNo.text.trim().isNotEmpty && regNo.value.text.trim() != "") ||
+              regNo.text.trim().length != 10;
       bool phoneNumberIsValid = phoneNumber.text.trim().isNotEmpty &&
           phoneNumber.value.text.trim() != "";
       bool emailIsValid =
