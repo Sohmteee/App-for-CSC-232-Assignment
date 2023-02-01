e277ece4f7a423c529a9ed0250f5ec09a8e2a61b
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-01 17:32:47 +0100

    commit

--- lib/screens/data_screen.dart
+++ lib/screens/data_screen.dart
@@ -19,7 +19,7 @@ class _DataScreenState extends State<DataScreen> {
   @override
   Widget build(BuildContext context) {
     studentsList = box.get("studentsList");
-    exportStudentsList(studentsList, "/storage");
+    exportStudentsList(studentsList);
 
     return Scaffold(
       body: SafeArea(
