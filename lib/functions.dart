83a01a72753487cd45ba17ec75377ee967f86f99
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-01 16:55:49 +0100

    commit

--- lib/functions.dart
+++ lib/functions.dart
@@ -27,9 +27,9 @@ mapToStudent(Map studentList) {
   );
 }
 
-void exportToCsv(List<Student> students) {
+void exportToCsv(List students) {
   final String csv = const ListToCsvConverter()
-      .convert([students.map((student) => student.toMap()).toList()]);
+      .convert([students.map((student) => mapToStudent(student).toMap()).toList()]);
 
   final Directory directory = Directory.systemTemp;
   final File file = File('${directory.path}/students.csv');
