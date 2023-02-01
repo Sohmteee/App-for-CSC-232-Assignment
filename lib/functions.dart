dcda28f3bc1818397e9d97e5ea1da269eb1f3585
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-01 14:22:40 +0100

    commit

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,6 +1,5 @@
 import 'data.dart';
 import 'student.dart';
-import 'dart:io';
 
 void clearInfo() {
   firstName.clear();
@@ -26,22 +25,3 @@ mapToStudent(Map studentList) {
   );
 }
 
-String studentsListToCsv(List<dynamic> studentsList) {
-  // Create the header row with the column names
-  final header = "Reg Number,Name,Sex,Department,State of Origin,Phone Number,Email Address";
-
-  // Map each student to a row in the CSV format
-  final rows = studentsList.map((student) {
-    final mappedStudent = mapToStudent(student);
-    return "${mappedStudent.regNumber},${mappedStudent.firstName} ${mappedStudent.lastName},${mappedStudent.sex},${mappedStudent.department},${mappedStudent.stateOfOrigin},${mappedStudent.phoneNumber},${mappedStudent.email}";
-  }).join("\n");
-
-  // Combine the header and rows to form the full CSV string
-  return "$header\n$rows";
-}
-
-// Call this function to get the CSV string
-final csv = studentsListToCsv(studentsList);
-
-// Write the CSV string to a file
-File("students.csv").writeAsString(csv);
