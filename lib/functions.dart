30a5253c2cc9212d9e4a8e5b649c9c2105b85c57
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-02-01 16:46:17 +0100

    commit

--- lib/functions.dart
+++ lib/functions.dart
@@ -26,3 +26,46 @@ mapToStudent(Map studentList) {
     email: studentList['email'],
   );
 }
+
+void exportToCsv() async {
+  // Get the list of students from the Hive box
+  var students = box.values.toList();
+
+  // Convert the students to maps so we can access their properties
+  List<Map> studentsList = students.map((e) => e.toMap()).toList();
+
+  // Convert the students to a list of lists
+  List<List<String>> csvData = [
+    [
+      'firstName',
+      'lastName',
+      'sex',
+      'department',
+      'stateOfOrigin',
+      'regNumber',
+      'phoneNumber',
+      'email'
+    ],
+  ];
+  studentsList.forEach((student) {
+    csvData.add([
+      student['firstName'],
+      student['lastName'],
+      student['sex'],
+      student['department'],
+      student['stateOfOrigin'],
+      student['regNumber'].toString(),
+      student['phoneNumber'],
+      student['email'],
+    ]);
+  });
+
+  // Create a CSV string from the data
+  String csv = const ListToCsvConverter().convert(csvData);
+
+  // Save the CSV string to a file on the device
+  final Directory dir = await getApplicationDocumentsDirectory();
+  final File file = File("${dir.path}/students.csv");
+  await file.writeAsString(csv);
+}
+
